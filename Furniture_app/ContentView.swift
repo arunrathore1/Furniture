f127d8e0521238512f7f439ff16181bdfd0fd08f
Arun Rathore <arathore@nowoptics.com>
2022-11-25 10:17:36 +0530

    Furniture App

---
+++ Furniture_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Furniture_app
+//
+//  Created by Arun Rathore on 30/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            HomeScreen()
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Furniture_app/Furniture_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_appApp.swift
+//  Furniture_app
+//
+//  Created by Arun Rathore on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
